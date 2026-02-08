c94bfd312527bca24f78f9a9ad22109247f17e41
bashamaliduwa-ux <bashamaliduwa@gmail.com>
2026-02-08 22:58:21 +0530

    Q4 (IT25101122)

---
+++ DrawFigures.java
@@ -0,0 +1,16 @@
+public class DrawFigures {
+   public static void main(String[] args) {
+       System.out.println("*****");
+       System.out.println("*****"); 
+       System.out.println("*****"); 
+
+       System.out.println(); 
+
+       System.out.println(" * "); 
+       System.out.println(" *** "); 
+       System.out.println("*****"); 
+
+   }
+
+}
+
