d1cd643187700443a1593f02b73ddcca3e3b79b0
bashamaliduwa-ux <bashamaliduwa@gmail.com>
2026-02-08 22:51:05 +0530

    Q.1 (IT25101122)

---
+++ MarathonDistance.java
@@ -0,0 +1,13 @@
+public class MarathonDistance{
+    public static void main(String[] args) {
+
+        int miles = 26;
+        int yards = 385;
+        double kilometers;
+
+        kilometers = (miles * 1.609) + (yards / 1760.0 * 1.609);
+
+        System.out.println("Distance in kilometers: " + kilometers);
+    }
+}
+
